f127fbc79db26fd39c596d940df2f318121b61fa
fdelafuentec <fdecanal22@jaumebalmes.net>
2023-02-08 20:44:47 +0100

    part Federico

---
+++ main.py
@@ -0,0 +1,7 @@
+from Fede import exercici1_B as ex1B
+from Fede import exercici2_B as ex2B
+from Fede import exercici3_B as ex3B
+
+print(ex1B.MesGranMesPetit())
+print(ex2B.ParellOSenar())
+print(ex3B.DeclaracioHisenda())
