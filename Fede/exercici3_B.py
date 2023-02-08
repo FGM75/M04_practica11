c7771d80b60d17326e11d6368dbba49d6779674c
fdelafuentec <fdecanal22@jaumebalmes.net>
2023-02-08 20:53:26 +0100

    Fede

--- Fede/exercici3_B.py
+++ Fede/exercici3_B.py
@@ -3,9 +3,7 @@ def DeclaracioHisenda ():
     edat = int(edat)
     ingresos = input("Escriu els teus ingresos: ")
     ingresos = int(ingresos)
-    if (edat >= 18) | (ingresos > 1200):
+    if (edat >= 18) and (ingresos > 1200):
         print("És necessari que facis la declaració d'hisenda")
     else:
         print("Encara no pots fe la declaració")
-
-    DeclaracioHisenda()
